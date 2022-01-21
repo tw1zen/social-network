e79a7cf3154c7198ca5efc8160b162c00bc76d9f
tw1zen <playtreborboy11@gmail.com>
2022-01-21 22:52:04 +0500

    add city and status

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,26 +1,29 @@
 import React from "react";
-import Mask from "../../img/mask.jpg"
-import './profile.css';
+import Mask from "../../img/mask.jpg";
+import "./profile.css";
 
-function Profile () {
-    return(
-        <div className="profile">
-            <div className="me">
-                <img src={Mask} alt="foto" />
-                <p>Ilon Mask</p>
-            </div>
-            <div className="posts">
-                <input type="text" />
-                <button>Добавить пост</button>
-                <div className="post">
-                    <img src={Mask} className="imgmini" />
-                    <span>Ilon Mask</span>
-                    <p>Hello, world!</p>
-                </div>
-            </div>
+function Profile() {
+  return (
+    <div className="profile">
+      <div className="me">
+        <img src={Mask} alt="foto" />
+        <div className="info">
+          <p>Ilon Mask</p>
+          <p>Town: Los Angeles</p>
+          <p>I owner of SpaceX</p>
         </div>
-    )
-
+      </div>
+      <div className="posts">
+        <input type="text" />
+        <button>Добавить пост</button>
+        <div className="post">
+          <img src={Mask} className="imgmini" />
+          <span>Ilon Mask</span>
+          <p>Hello, world!</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Profile
+export default Profile;
